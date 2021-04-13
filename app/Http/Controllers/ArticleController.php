b37bd4cf65f0574d580e962249efd06c32b80b91
vegaangga <vegaangga27@gmail.com>
2021-04-13 23:42:14 +0700

    Prak 2 Langkah 3 Modifikasi fungsi update pada ArticleController agar dapat berungsi untuk mengubah data

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Article;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ArticleController extends Controller
 {
@@ -67,9 +68,9 @@ class ArticleController extends Controller
      * @param  \App\Models\Article  $article
      * @return \Illuminate\Http\Response
      */
-    public function edit(Article $article)
+    public function edit($id)
     {
-        //
+        
     }
 
     /**
@@ -79,9 +80,22 @@ class ArticleController extends Controller
      * @param  \App\Models\Article  $article
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Article $article)
+    public function update(Request $request, $id)
     {
-        //
+        $article = Article::find($id);
+
+        $article->title = $request->title;
+        $article->content = $request->content;
+
+        if($article->featured_image && file_exists(storage_path('app/public'.$article->featured_image))){
+            Storage::delete('public/'.$article->featured_image);
+        }
+
+        $image_name = $request->file('upload')->store('images','public');
+        $article->featured_image = $image_name;
+
+        $article->save();
+        return 'Artikel berhasil diubah';
     }
 
     /**
