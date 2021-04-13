cbda0b213469a52fc8112ae1b19deb15d13e75eb
vegaangga <vegaangga27@gmail.com>
2021-04-14 00:39:39 +0700

    Prak 3 Langkah 4 Menambahkan funsgi cetak_pdf pada ArticleController

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use PDF;
 use App\Models\Article;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
@@ -109,5 +109,13 @@ class ArticleController extends Controller
     public function destroy(Article $article)
     {
         //
+    }
+
+    public function cetak_pdf(){
+        $articles = Article::all();
+
+        $pdf = PDF::loadView('articles.articles_pdf',['articles'=>$articles]);
+
+        return $pdf->stream();
     }
 }
