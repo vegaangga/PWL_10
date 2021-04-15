19431b8c887ed475e55c5b2315966d8fb3be42f1
vegaangga <vegaangga27@gmail.com>
2021-04-15 21:17:50 +0700

    Tugas Praktikum 1- Menambahkan Fitur Upload Menambahkan kolom pada index.blade.php

--- resources/views/mahasiswas/index.blade.php
+++ resources/views/mahasiswas/index.blade.php
@@ -35,6 +35,7 @@
     <tr>
         <th>Nim</th>
         <th>Nama</th>
+        <th>Foto</th>
         <th>Kelas</th>
         <th>Jurusan</th>
         <th>No_Handphone</th>
@@ -44,6 +45,7 @@
     <tr>
         <td>{{ $Mahasiswa->nim }}</td>
         <td>{{ $Mahasiswa->nama }}</td>
+        <td><img width="100px" src="{{asset('storage/'.$Mahasiswa->foto_profile)}}"></td>
         <td>{{ $Mahasiswa->kelas->nama_kelas }}</td>
         <td>{{ $Mahasiswa->jurusan }}</td>
         <td>{{ $Mahasiswa->no_handphone }}</td>
